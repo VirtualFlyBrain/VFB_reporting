3ca84fa1b16fc6d87326b4bf26787e5d06f97509
Rob Court <rcourt@ed.ac.uk>
2019-07-11 17:21:35 +0100

    location fix

--- src/report_runner.py
+++ src/report_runner.py
@@ -4,7 +4,7 @@ from reporting_tools import diff_report, gen_dataset_report, gen_dataset_report_
 kb_report = gen_dataset_report(["http://kb.virtualflybrain.org", "neo4j", "neo4j"], 'kb')
 print(kb_report.name)
 
-save_report(kb_report, "results/kb_report.tsv")
+save_report(kb_report, "../results/kb_report.tsv")
 
 pdb_report = gen_dataset_report_prod(["http://pdb.virtualflybrain.org", "neo4j", "neo4j"], 'pdb')
 
@@ -17,7 +17,7 @@ dev_diff = diff_report(pdb_report, dev_report)
 print(type(staging_diff))
 print(type(dev_diff))
 
-save_report(staging_diff.staging_not_pdb, 'results/staging_diff.tsv')
-save_report(dev_diff.dev_not_pdb, 'results/dev_diff.tsv')
+save_report(staging_diff.staging_not_pdb, '../results/staging_diff.tsv')
+save_report(dev_diff.dev_not_pdb, '../results/dev_diff.tsv')
 
 
