56c1d6e362ffb201fe9bc52f2b51192b5f9cdf2a
Rob Court <rcourt@ed.ac.uk>
2019-07-11 17:18:29 +0100

    adding basic push script

---
+++ src/push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git add . results/*.tsv
+  echo "Results from latest Travis build: $TRAVIS_BUILD_NUMBER" > reports/readme.md
+  git add . results/readme.md
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git push --quiet
+}
+
+setup_git
+commit_website_files
+upload_files
