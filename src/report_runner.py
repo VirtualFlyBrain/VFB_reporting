9520af74bb753266c4332ec33cdd075d5c8bd54c
dosumis <dosumis@gmail.com>
2019-07-17 13:44:13 +0100

    Creating full set of reports.

--- src/report_runner.py
+++ src/report_runner.py
@@ -2,20 +2,20 @@ from reporting_tools import diff_report, gen_dataset_report, gen_dataset_report_
 
 
 kb_report = gen_dataset_report(["http://kb.virtualflybrain.org", "neo4j", "neo4j"], 'kb')
-print(kb_report.name)
 
 save_report(kb_report, "../VFB_reporting_results/kb_report.tsv")
 
 pdb_report = gen_dataset_report_prod(["http://pdb.virtualflybrain.org", "neo4j", "neo4j"], 'pdb')
+save_report(pdb_report, "../VFB_reporting_results/pdb_report.tsv")
 
 staging_report = gen_dataset_report_prod(["http://pdb-alpha.virtualflybrain.org", "neo4j", "neo4j"], 'staging')
+save_report(staging_report, "../VFB_reporting_results/staging_report.tsv")
 
 dev_report = gen_dataset_report_prod(["http://pdb-dev.virtualflybrain.org", "neo4j", "neo4j"], 'dev')
+save_report(dev_report, "../VFB_reporting_results/dev_report.tsv")
 
 staging_diff = diff_report(pdb_report, staging_report)
 dev_diff = diff_report(pdb_report, dev_report)
-print(type(staging_diff))
-print(type(dev_diff))
 
 save_report(staging_diff.staging_not_pdb, '../VFB_reporting_results/staging_diff.tsv')
 save_report(dev_diff.dev_not_pdb, '../VFB_reporting_results/dev_diff.tsv')
