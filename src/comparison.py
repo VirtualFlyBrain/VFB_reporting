9d1826694eb22415369b0788b8495806b6e30ac6
Clare72 <cp390@cam.ac.uk>
2019-08-05 12:39:55 +0100

    made functions to reduce need for intermediary tables

--- src/comparison.py
+++ src/comparison.py
@@ -1,65 +1,81 @@
 import pandas as pd
 from uk.ac.ebi.vfb.neo4j.neo4j_tools import neo4j_connect, results_2_dict_list
+import get_catmaid_papers
+
 nc = neo4j_connect('http://kb.virtualflybrain.org', 'neo4j', 'neo4j')
 
-CAT_papers = pd.read_csv("CAT_datasets.tsv", sep="\t", index_col="id")
-CAT_skids = pd.read_csv("CAT_skids.tsv", sep="\t")
+L1_papers = get_catmaid_papers.gen_cat_paper_report(
+    "https://l1em.catmaid.virtualflybrain.org", 1, "papers", "L1_CAT")
+L1_skids = get_catmaid_papers.gen_cat_skid_report(
+    "https://l1em.catmaid.virtualflybrain.org", 1, "papers", "L1_CAT")
+'''FAFB_papers = get_catmaid_papers.gen_cat_paper_report(
+    "https://fafb.catmaid.virtualflybrain.org", 1, "papers", "FAFB_CAT")
+FAFB_skids = get_catmaid_papers.gen_cat_skid_report(
+    "https://fafb.catmaid.virtualflybrain.org", 1, "papers", "FAFB_CAT")'''
 
-# Get table of names of catmaid datasets in VFB
-pub_query = "MATCH (ds:DataSet) WHERE ds.catmaid_annotation_id IS NOT NULL " \
-            "RETURN ds.catmaid_annotation_id as CATMAID_ID, ds.short_form as VFB_name"
-q = nc.commit_list([pub_query])
-papers = results_2_dict_list(q)
 
-VFB_papers = pd.DataFrame.from_dict(papers)
-VFB_papers = VFB_papers.set_index("CATMAID_ID")
+def make_catmaid_vfb_reports(cat_papers, cat_skids, dataset_name):
+    """Return a comparison with data in VFB for given sets of papers and skids in CATMAID"""
+    # Get table of names of catmaid datasets in VFB
+    pub_query = "MATCH (ds:DataSet) WHERE ds.catmaid_annotation_id IS NOT NULL " \
+                "RETURN ds.catmaid_annotation_id as CATMAID_ID, ds.short_form as VFB_name"
+    q = nc.commit_list([pub_query])
+    papers = results_2_dict_list(q)
 
-# match up SKIDs per paper and output dict of lists of skids
-skids_by_paper = {}
-for paper_id in CAT_papers.index:  # do everything per paper
+    vfb_papers = pd.DataFrame.from_dict(papers)
+    vfb_papers = vfb_papers.set_index("CATMAID_ID")
 
-    # get list skids in CATMAID data as strings
-    skids_in_paper_CAT = [str(s) for s in CAT_skids[CAT_skids['paper_id'] == paper_id]['skid']]
+    # match up SKIDs per paper and output dict of lists of skids
+    skids_by_paper = {}
+    for paper_id in cat_papers.index:  # do everything per paper
 
-    # get skids from VFB KB and reformat to list of strings
-    query = "MATCH (ds:DataSet {catmaid_annotation_id : " + str(paper_id) + \
-            "})<-[:has_source]-()<-[]-()-[r:in_register_with]->() WHERE r.catmaid_skeleton_ids" \
-            " IS NOT NULL RETURN DISTINCT r.catmaid_skeleton_ids"
-    q = nc.commit_list([query])
-    skids_in_paper_VFB = results_2_dict_list(q)
+        # get list of skids in CATMAID data as strings
+        skids_in_paper_cat = [str(s) for s in cat_skids[cat_skids['paper_id'] == paper_id]['skid']]
 
-    skids_in_paper_VFB = [s['r.catmaid_skeleton_ids'] for s in skids_in_paper_VFB]  # flatten to list
-    skids_in_paper_VFB = [s.replace("[", "") for s in skids_in_paper_VFB]  # remove brackets
-    skids_in_paper_VFB = [s.replace("]", "") for s in skids_in_paper_VFB]  # remove brackets
+        # get skids from VFB KB and reformat to list of strings
+        query = "MATCH (ds:DataSet {catmaid_annotation_id : " + str(paper_id) + \
+                "})<-[:has_source]-()<-[]-()-[r:in_register_with]->() WHERE r.catmaid_skeleton_ids" \
+                " IS NOT NULL RETURN DISTINCT r.catmaid_skeleton_ids"
+        q = nc.commit_list([query])
+        skids_in_paper_vfb = results_2_dict_list(q)
 
-    # comparison of lists of skids
-    cat_not_vfb = [s for s in skids_in_paper_CAT if s not in skids_in_paper_VFB]
-    vfb_not_cat = [s for s in skids_in_paper_VFB if s not in skids_in_paper_CAT]
-    skids_by_paper[paper_id] = {'skids_in_paper_CAT': skids_in_paper_CAT, 'skids_in_paper_VFB': skids_in_paper_VFB,
-                                'cat_not_vfb': cat_not_vfb, 'vfb_not_cat': vfb_not_cat}
+        skids_in_paper_vfb = [s['r.catmaid_skeleton_ids'] for s in skids_in_paper_vfb]  # flatten to list
+        skids_in_paper_vfb = [s.replace("[", "") for s in skids_in_paper_vfb]  # remove brackets
+        skids_in_paper_vfb = [s.replace("]", "") for s in skids_in_paper_vfb]  # remove brackets
 
-# make dataframe of list lengths from skid_diff
-skids_df = pd.DataFrame.from_dict(skids_by_paper, orient='index')
-skids_df = skids_df.applymap(lambda x: len(x))
+        # comparison of lists of skids
+        cat_not_vfb = [s for s in skids_in_paper_cat if s not in skids_in_paper_vfb]
+        vfb_not_cat = [s for s in skids_in_paper_vfb if s not in skids_in_paper_cat]
+        skids_by_paper[paper_id] = {'skids_in_paper_cat': skids_in_paper_cat, 'skids_in_paper_vfb': skids_in_paper_vfb,
+                                    'cat_not_vfb': cat_not_vfb, 'vfb_not_cat': vfb_not_cat}
 
-# make combined table with all info, tidy up and save as tsv
-all_papers = pd.concat([CAT_papers, VFB_papers, skids_df], join="outer", axis=1, sort=True)
-all_papers.rename(columns={'name': 'CATMAID_name', 'VFB_name': 'VFB_name',
-                           'skids_in_paper_CAT': 'CATMAID_SKIDs', 'skids_in_paper_VFB': 'VFB_SKIDS',
-                           'cat_not_vfb': 'CATMAID_not_VFB', 'vfb_not_cat': 'VFB_not_CATMAID'},
-                  inplace=True)
-all_papers.index.name = 'Paper_ID'
-all_papers.to_csv("all_papers.tsv", sep="\t")
+    # make dataframe of list lengths from skid_diff
+    skids_df = pd.DataFrame.from_dict(skids_by_paper, orient='index')
+    skids_df = skids_df.applymap(lambda x: len(x))
 
-# TODO - make table of skids requiring mapping
+    # make combined table with all info, tidy up and save as tsv
+    all_papers = pd.concat([cat_papers, vfb_papers, skids_df], join="outer", axis=1, sort=True)
+    all_papers.rename(columns={'name': 'CATMAID_name', 'VFB_name': 'VFB_name',
+                               'skids_in_paper_cat': 'CATMAID_SKIDs', 'skids_in_paper_vfb': 'VFB_SKIDS',
+                               'cat_not_vfb': 'CATMAID_not_VFB', 'vfb_not_cat': 'VFB_not_CATMAID'},
+                      inplace=True)
+    all_papers.index.name = 'Paper_ID'
+    all_papers.to_csv(dataset_name + "_comparison.tsv", sep="\t")
 
-# TERMINAL OUTPUT
+    # TODO - make table of skids requiring mapping
 
-# new papers
-new_papers = all_papers[all_papers.VFB_name.isnull()]
-print(str(len(new_papers.index)) + " new papers in CATMAID that are not in VFB")
-print(new_papers["CATMAID_name"])
-print("See all_papers.tsv for differences in numbers of SKIDs")
+    # TERMINAL OUTPUT
+
+    # new papers
+    new_papers = all_papers[all_papers.VFB_name.isnull()]
+    print(str(len(new_papers.index)) + " new papers in CATMAID that are not in VFB")
+    print(new_papers["CATMAID_name"])
+    print("See " + dataset_name + "_comparison.tsv for differences in numbers of SKIDs")
+
+
+make_catmaid_vfb_reports(L1_papers, L1_skids, "L1EM")
+
+
 
 """
 # SKID details - OBSOLETE?
