1c30f8450827c6e4f93db7845bfde0aca1fd5505
Clare72 <cp390@cam.ac.uk>
2019-08-05 12:41:26 +0100

    made functions to reduce need for intermediary table

--- src/get_catmaid_papers.py
+++ src/get_catmaid_papers.py
@@ -2,10 +2,11 @@ import requests
 import pandas as pd
 from collections import OrderedDict
 
-def gen_cat_report(URL, PROJECT_ID, paper_annotaion, report_name):
 
-    dataset_outfile = "../VFB_reporting_results/" + report_name + "_datasets.tsv"
-    skid_outfile = "../VFB_reporting_results/" + report_name + "_skids.tsv"
+def gen_cat_paper_report(URL, PROJECT_ID, paper_annotaion, report_name):
+
+    # dataset_outfile = "../VFB_reporting_results/" + report_name + "_datasets.tsv"
+    # skid_outfile = "../VFB_reporting_results/" + report_name + "_skids.tsv"
     
     # get token
 
@@ -29,10 +30,41 @@ def gen_cat_report(URL, PROJECT_ID, paper_annotaion, report_name):
     df_papers = df_papers.set_index("id")
     df_papers = df_papers.drop("type", axis=1)
     df_papers = df_papers.sort_values("name")
-    df_papers.to_csv(dataset_outfile, sep="\t")
+    # df_papers.to_csv(dataset_outfile, sep="\t")
+    return df_papers
 
     # SKIDs
     # pull out neuron details for all papers
+
+
+def gen_cat_skid_report(URL, PROJECT_ID, paper_annotaion, report_name):
+
+    # dataset_outfile = "../VFB_reporting_results/" + report_name + "_datasets.tsv"
+    # skid_outfile = "../VFB_reporting_results/" + report_name + "_skids.tsv"
+
+    # get token
+
+    client = requests.session()
+    client.get("%s" % URL)
+    for key in client.cookies.keys():
+        if key[:4] == 'csrf':
+            csrf_key = key
+    csrftoken = client.cookies[csrf_key]
+
+    print(csrftoken)
+
+    # PAPERs
+    # pull out paper ids and names -> table and save as tsv
+
+    call_papers = {"annotated_with": paper_annotaion, "with_annotations": False, "annotation_reference": "name"}
+    papers = client.post("%s/%d/annotations/query-targets" % (URL, PROJECT_ID),
+                         data=call_papers, headers={"Referer": URL, "X-CSRFToken": csrftoken}).json()["entities"]
+
+    df_papers = pd.DataFrame(papers)
+    df_papers = df_papers.set_index("id")
+    df_papers = df_papers.drop("type", axis=1)
+    df_papers = df_papers.sort_values("name")
+    # df_papers.to_csv(dataset_outfile, sep="\t")
 
     call_papers["annotation_reference"] = "id"
     for paper in papers:
@@ -57,8 +89,9 @@ def gen_cat_report(URL, PROJECT_ID, paper_annotaion, report_name):
                 df_skids = pd.concat([df_skids, df_row])
 
     df_skids = df_skids.sort_values(["paper_name","skid"])
-    df_skids.to_csv(skid_outfile, sep="\t", index=False)
+    # df_skids.to_csv(skid_outfile, sep="\t", index=False)
+    return df_skids
 
     
-gen_cat_report("https://l1em.catmaid.virtualflybrain.org",1,"papers","L1_CAT")
-gen_cat_report("https://fafb.catmaid.virtualflybrain.org",1,"Published","FAFB_CAT")
+gen_cat_paper_report("https://l1em.catmaid.virtualflybrain.org", 1, "papers", "L1_CAT")
+gen_cat_paper_report("https://fafb.catmaid.virtualflybrain.org", 1, "Published", "FAFB_CAT")
