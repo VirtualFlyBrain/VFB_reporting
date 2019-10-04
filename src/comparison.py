739572dc0182c760cdbe27443144320ab8ea9eee
Rob Court <rcourt@ed.ac.uk>
2019-10-04 17:40:09 +0100

    Update comparison.py

--- src/comparison.py
+++ src/comparison.py
@@ -18,11 +18,11 @@ FAFB = [get_catmaid_papers.gen_cat_paper_report(
     "https://fafb.catmaid.virtualflybrain.org", 1, "Published", "neuron name", "FAFB_CAT"),
     "FAFB"]
 
-VNC1 = [get_catmaid_papers.gen_cat_paper_report(
-    "https://vnc1.catmaid.virtualflybrain.org", 1, "Published", "VNC1_CAT"),
-    get_catmaid_papers.gen_cat_skid_report_officialnames(
-    "https://fafb.catmaid.virtualflybrain.org", 1, "Published", "neuron name", "VNC1_CAT"),
-    "VNC1"]
+#VNC1 = [get_catmaid_papers.gen_cat_paper_report(
+#    "https://vnc1.catmaid.virtualflybrain.org", 1, "Published", "VNC1_CAT"),
+#    get_catmaid_papers.gen_cat_skid_report_officialnames(
+#    "https://fafb.catmaid.virtualflybrain.org", 1, "Published", "neuron name", "VNC1_CAT"),
+#    "VNC1"]
 
 
 def make_catmaid_vfb_reports(cat_papers, cat_skids, dataset_name):
