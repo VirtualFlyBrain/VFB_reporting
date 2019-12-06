1abff9363919a01c6f62048697267e9591319df7
dosumis <dosumis@gmail.com>
2019-12-06 15:00:52 +0000

    Fixed skid/typing query (accession is int).

--- src/comparison.py
+++ src/comparison.py
@@ -72,7 +72,7 @@ def make_catmaid_vfb_reports(cat_papers, cat_skids, dataset_name):
                 "-[skid:hasDbXref]->(s:Site) " \
                 "WHERE api.short_form in ['fafb_catmaid_api', 'l1em_catmaid_api'] " \
                 "AND s.short_form in ['catmaid_fafb', 'catmaid_l1em'] " \
-                "AND dsxref.accession = '" + str(paper_id) +"' WITH i, skid " \
+                "AND dsxref.accession = " + str(paper_id) +" WITH i, skid " \
                 "MATCH (i)-[:INSTANCEOF]-(c:Class) " \
                 "RETURN distinct skid.accession AS `r.catmaid_skeleton_ids`, c.iri"
 
