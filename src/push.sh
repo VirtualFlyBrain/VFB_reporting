fc86ee76c57c4997302afaa7ca7841a1d25b4d86
Rob Court <rcourt@ed.ac.uk>
2019-12-09 12:35:22 +0000

    changing to match DOS version

--- src/push.sh
+++ src/push.sh
@@ -7,7 +7,7 @@ setup_git() {
 
 commit_website_files() {
   git add . *.tsv
-  echo "# VFB_reporting_results\nrepository containing results of various data change and consistency checking.\n\n Current results are from the latest travis build #$TRAVIS_BUILD_NUMBER from commit: '$TRAVIS_COMMIT_MESSAGE' on $TRAVIS_BRANCH\n\n" > README.md
+  echo "# VFB_reporting_results\nRepo for the results of pipelines reporting dataflow to and within VFB.\n\n Current results are from the latest travis build #$TRAVIS_BUILD_NUMBER from commit: '$TRAVIS_COMMIT_MESSAGE' on $TRAVIS_BRANCH\n\n" > README.md
   cat ../reports.md >> README.md
   git add . README.md
   git commit --message "Travis build: $TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER "$(date "+%Y%m%d-%H%M%S") 
