9c82988ba50fc01a1114abd1cbd532907275eb5e
Rob Court <rcourt@ed.ac.uk>
2020-04-14 15:07:23 +0100

    Handling intermittent pdb-dev/alpha servers

--- src/report_runner.py
+++ src/report_runner.py
@@ -8,16 +8,23 @@ save_report(kb_report, "../VFB_reporting_results/kb_report.tsv")
 pdb_report = gen_dataset_report_prod(["http://pdb.virtualflybrain.org", "neo4j", "neo4j"], 'pdb')
 save_report(pdb_report, "../VFB_reporting_results/pdb_report.tsv")
 
-staging_report = gen_dataset_report_prod(["http://pdb-alpha.virtualflybrain.org", "neo4j", "neo4j"], 'staging')
-save_report(staging_report, "../VFB_reporting_results/staging_report.tsv")
+try:
+  staging_report = gen_dataset_report_prod(["http://pdb-alpha.virtualflybrain.org", "neo4j", "neo4j"], 'staging')
+  save_report(staging_report, "../VFB_reporting_results/staging_report.tsv")
+  staging_diff = diff_report(pdb_report, staging_report)
+  save_report(staging_diff.staging_not_pdb, '../VFB_reporting_results/staging_diff.tsv')
+except:
+  print("An exception occurred running staging report!")
 
-dev_report = gen_dataset_report_prod(["http://pdb-dev.virtualflybrain.org", "neo4j", "neo4j"], 'dev')
-save_report(dev_report, "../VFB_reporting_results/dev_report.tsv")
+try:
+  dev_report = gen_dataset_report_prod(["http://pdb-dev.virtualflybrain.org", "neo4j", "neo4j"], 'dev')
+  save_report(dev_report, "../VFB_reporting_results/dev_report.tsv")
+  dev_diff = diff_report(pdb_report, dev_report)
+  save_report(dev_diff.dev_not_pdb, '../VFB_reporting_results/dev_diff.tsv')
+except:
+  print("An exception occurred running dev report!")
+  
 
-staging_diff = diff_report(pdb_report, staging_report)
-dev_diff = diff_report(pdb_report, dev_report)
 
-save_report(staging_diff.staging_not_pdb, '../VFB_reporting_results/staging_diff.tsv')
-save_report(dev_diff.dev_not_pdb, '../VFB_reporting_results/dev_diff.tsv')
 
 
