b5275e69f41cf329422b2418d11551ffcc942580
Clare72 <cp390@cam.ac.uk>
2021-03-25 13:59:36 +0000

    fixed text string

--- src/reporting_tools.py
+++ src/reporting_tools.py
@@ -33,20 +33,19 @@ def gen_report(server, query, report_name, column_order=None):
 def gen_dataset_report(server,
                        report_name,
                        production_only=False):
-
     qr = ("MATCH (ds:DataSet) with ds "
-    "OPTIONAL MATCH (ds)-[:has_reference]->(p:pub) "
-    "WITH ds, p "
-    "OPTIONAL MATCH (ds)-[:has_license|license]->(l:License) "
-    "WITH ds, p, l "
-    "OPTIONAL MATCH (ds)<-[:has_source]-(i:Individual) "
-    " RETURN ds.short_form, ds.label, ds.production[0] as `ds.production`, "
-    "l.label as license,  p.short_form as pub, "
-    "count(i) as individuals order by ds.short_form")
+          "OPTIONAL MATCH (ds)-[:has_reference]->(p:pub) "
+          "WITH ds, p "
+          "OPTIONAL MATCH (ds)-[:has_license|license]->(l:License) "
+          "WITH ds, p, l "
+          "OPTIONAL MATCH (ds)<-[:has_source]-(i:Individual) "
+          " RETURN ds.short_form, ds.label, ds.production[0] as `ds.production`, "
+          "l.label as license,  p.short_form as pub, "
+          "count(i) as individuals order by ds.short_form")
     if production_only:
         qr = qr.replace("MATCH (ds:DataSet) with ds ", "MATCH (ds:DataSet) with ds WHERE ds.production[0] = true ")
     if 'KB' in server[0].upper():
-        qr = qr.replace("production[0]","production")
+        qr = qr.replace("production[0]", "production")
     return gen_report(
         server,
         query=qr,
@@ -61,21 +60,20 @@ def gen_dataset_report(server,
 def gen_dataset_report_prod(server, report_name):
     return gen_report(
         server,
-        query="MATCH (ds:DataSet) WHERE ds.production[0] = true WITH ds "
-              "OPTIONAL MATCH (ds)-[:has_reference]->(p:pub) "
-              "WITH ds, p "
-              "OPTIONAL MATCH (a:Class)-[:has_reference]->(p) "
-              "WITH ds, p, count(distinct a) as ontology_terms "
-              "OPTIONAL MATCH (:Individual)-[r:overlaps { pub: p.short_form}]->(:Expression_pattern) "
-              "WITH ds, p, ontology_terms, COUNT (distinct r) as exp_cur "
-              "OPTIONAL MATCH (ds)-[:
-        ]->(l:License) "
-              "WITH ds, p, exp_cur, ontology_terms, l "
-              "OPTIONAL MATCH (ds)<-[:has_source]-(i:Individual) "
-              "RETURN ds.short_form, ds.label,"
-              "l.label as license, p.short_form as pub, "
-              "count(distinct i) as individuals, exp_cur, ontology_terms "
-              "order by ds.short_form",
+        query=("MATCH (ds:DataSet) WHERE ds.production[0] = true WITH ds "
+               "OPTIONAL MATCH (ds)-[:has_reference]->(p:pub) "
+               "WITH ds, p "
+               "OPTIONAL MATCH (a:Class)-[:has_reference]->(p) "
+               "WITH ds, p, count(distinct a) as ontology_terms "
+               "OPTIONAL MATCH (:Individual)-[r:overlaps { pub: p.short_form}]->(:Expression_pattern) "
+               "WITH ds, p, ontology_terms, COUNT (distinct r) as exp_cur "
+               "OPTIONAL MATCH (ds)-[:]->(l:License) "
+               "WITH ds, p, exp_cur, ontology_terms, l "
+               "OPTIONAL MATCH (ds)<-[:has_source]-(i:Individual) "
+               "RETURN ds.short_form, ds.label,"
+               "l.label as license, p.short_form as pub, "
+               "count(distinct i) as individuals, exp_cur, ontology_terms "
+               "order by ds.short_form"),
         report_name=report_name, column_order=['ds.short_form',
                                                'ds.label',
                                                'license',
@@ -83,7 +81,6 @@ def gen_dataset_report_prod(server, report_name):
                                                'individuals',
                                                'ontology_terms',
                                                'exp_cur'])
-
 
 
 def diff_report(report1: pd.DataFrame, report2: pd.DataFrame):
@@ -94,15 +91,9 @@ def diff_report(report1: pd.DataFrame, report2: pd.DataFrame):
     left_only = merged[merged['_merge'] == 'left_only']
     right_only = merged[merged['_merge'] == 'right_only']
     out = {report1.name + '_not_' + report2.name: left_only.drop(columns=['_merge']),
-             report2.name + '_not_' + report1.name: right_only.drop(columns=['_merge'])}
+           report2.name + '_not_' + report1.name: right_only.drop(columns=['_merge'])}
     return namedtuple('out', out.keys())(*out.values())
 
 
 def save_report(report, filename):
     report.to_csv(filename, sep='\t', index=False)
-
-
-
-
-
-
