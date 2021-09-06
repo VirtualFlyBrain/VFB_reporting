40ece6c4f853d53d398ad769510155d905d6f8d1
Rob Court <rcourt@ed.ac.uk>
2021-09-06 14:01:21 +0100

    adding pipeline output

--- src/report_runner.py
+++ src/report_runner.py
@@ -9,6 +9,14 @@ pdb_report = gen_dataset_report_prod(["http://pdb.virtualflybrain.org", "neo4j",
 save_report(pdb_report, "../VFB_reporting_results/pdb_report.tsv")
 
 try:
+  pipeline_output_report = gen_dataset_report_prod(["http://pdb.p2.virtualflybrain.org", "neo4j", "neo4j"], 'staging')
+  save_report(pipeline_output_report, "../VFB_reporting_results/pipeline_output_report.tsv")
+  pipeline_output_diff = diff_report(pdb_report, pipeline_output_report)
+  save_report(pipeline_output_diff.pipeline_output_not_pdb, '../VFB_reporting_results/pipeline_output_diff.tsv')
+except:
+  print("An exception occurred running pipeline output report!")
+
+try:
   staging_report = gen_dataset_report_prod(["http://pdb-alpha.virtualflybrain.org", "neo4j", "neo4j"], 'staging')
   save_report(staging_report, "../VFB_reporting_results/staging_report.tsv")
   staging_diff = diff_report(pdb_report, staging_report)
