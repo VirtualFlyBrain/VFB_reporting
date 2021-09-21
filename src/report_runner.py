971e51cc1dbd2485dde575cd2271fb556d0b7066
Rob Court <rcourt@ed.ac.uk>
2021-09-21 21:38:41 +0100

    resetting default password

--- src/report_runner.py
+++ src/report_runner.py
@@ -1,15 +1,15 @@
 from reporting_tools import diff_report, gen_dataset_report, gen_dataset_report_prod, save_report
 
 
-kb_report = gen_dataset_report(["http://kb.virtualflybrain.org", "neo4j", "neo4j"], 'kb')
+kb_report = gen_dataset_report(["http://kb.virtualflybrain.org", "neo4j", "vfb"], 'kb')
 save_report(kb_report, "../VFB_reporting_results/kb_report.tsv")
 
-pdb_report = gen_dataset_report_prod(["http://pdb.virtualflybrain.org", "neo4j", "neo4j"], 'pdb')
+pdb_report = gen_dataset_report_prod(["http://pdb.virtualflybrain.org", "neo4j", "vfb"], 'pdb')
 save_report(pdb_report, "../VFB_reporting_results/pdb_report.tsv")
 
 
 try:
-  kb2_report = gen_dataset_report(["http://kb2.virtualflybrain.org", "neo4j", "neo4j"], 'kb2')
+  kb2_report = gen_dataset_report(["http://kb2.virtualflybrain.org", "neo4j", "vfb"], 'kb2')
   save_report(kb2_report, "../VFB_reporting_results/kb2_report.tsv")
   kb2_diff = diff_report(kb_report, kb2_report)
   save_report(kb2_diff, '../VFB_reporting_results/kb_kb2_diff.tsv')
@@ -18,7 +18,7 @@ except:
 
 
 try:
-  pipeline_output_report = gen_dataset_report_prod(["http://pdb.p2.virtualflybrain.org", "neo4j", "neo4j"], 'pipeline_output')
+  pipeline_output_report = gen_dataset_report_prod(["http://pdb.p2.virtualflybrain.org", "neo4j", "vfb"], 'pipeline_output')
   save_report(pipeline_output_report, "../VFB_reporting_results/pipeline_output_report.tsv")
   pipeline_output_diff = diff_report(pdb_report, pipeline_output_report)
   save_report(pipeline_output_diff, '../VFB_reporting_results/pdb_pipeline_output_diff.tsv')
@@ -26,7 +26,7 @@ except:
   print("An exception occurred running pipeline output report!")
 
 try:
-  staging_report = gen_dataset_report_prod(["http://pdb-alpha.virtualflybrain.org", "neo4j", "neo4j"], 'staging')
+  staging_report = gen_dataset_report_prod(["http://pdb-alpha.virtualflybrain.org", "neo4j", "vfb"], 'staging')
   save_report(staging_report, "../VFB_reporting_results/staging_report.tsv")
   staging_diff = diff_report(pdb_report, staging_report)
   save_report(staging_diff, '../VFB_reporting_results/pdb_staging_diff.tsv')
@@ -34,7 +34,7 @@ except:
   print("An exception occurred running staging report!")
 
 try:
-  dev_report = gen_dataset_report_prod(["http://pdb-dev.virtualflybrain.org", "neo4j", "neo4j"], 'dev')
+  dev_report = gen_dataset_report_prod(["http://pdb-dev.virtualflybrain.org", "neo4j", "vfb"], 'dev')
   save_report(dev_report, "../VFB_reporting_results/dev_report.tsv")
   dev_diff = diff_report(pdb_report, dev_report)
   save_report(dev_diff, '../VFB_reporting_results/pdb_dev_diff.tsv')
