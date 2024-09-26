88f75f1f5d701b671d3f0032ce955cdf115dd3ff
Clare72 <cp390@cam.ac.uk>
2024-09-26 18:50:06 +0100

    Update newmeta_curation_file_maker.py

--- src/make_curation_records/newmeta_curation_file_maker.py
+++ src/make_curation_records/newmeta_curation_file_maker.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import datetime
-import numpy
-from vfb_connect.neo.neo4j_tools import Neo4jConnect, dict_cursor
+from vfb_connect import vfb
 
-nc = Neo4jConnect('http://kb.virtualflybrain.org', 'neo4j', 'vfb')
+vfb.setNeoEndpoint('http://kb.virtualflybrain.org', usr='neo4j', pwd='vfb')
 curator = 'cp390'  # change if needed
 
 """
-Makes newmeta curation record(s) for skids in VFB_reporting_results/FAFB_CAT_cellType_skids.tsv.
-Mappings come from Management/FAFB/
+Makes newmeta curation record(s) for skids in VFB_reporting_results/FAFB_all_skids_officialnames.tsv 
+where typings are present in Management/FAFB/FAFB_skid_FBbt.tsv.
 Records should be transferred to curation repo for loading.
 Run anat_curation_file_maker.py first to get any new skids into VFB.
 Only adds new (not in VFB) is_a annotations.
@@ -20,18 +19,17 @@ today = datetime.date.today()
 datestring = today.strftime("%y%m%d")
 
 # open file of new FAFB skids (no paper ids) - from Management not Reporting results
-#typed_skids = pd.read_csv("../../../VFB_reporting_results/FAFB_CAT_cellType_skids.tsv",
-#                          sep='\t').applymap(str)
 typed_skids = pd.read_csv("../../../Management/FAFB/FAFB_skid_FBbt.tsv",
-                          sep='\t').dropna().applymap(str)
+                          sep='\t', dtype='str')
+typed_skids = typed_skids[typed_skids['FBbt_id'].notna()]
 
 # open file of all skids, including paper ids
 all_skids = pd.read_csv("../../../VFB_reporting_results/CATMAID_SKID_reports/FAFB_all_skids_officialnames.tsv",
-                        sep='\t').applymap(str)
+                        sep='\t', dtype='str')
 
 # get mapping of dataset name (in VFB) to id (as index) from FAFB_comparison.tsv
 comparison_table = pd.read_csv("../../../VFB_reporting_results/CATMAID_SKID_reports/FAFB_comparison.tsv",
-                               sep='\t', index_col='Paper_ID').applymap(str)
+                               sep='\t', index_col='Paper_ID', dtype='str')
 comparison_table.index = comparison_table.index.map(str)
 
 # merge info from all skids into typed skids (by skid), remove excess columns
@@ -39,54 +37,45 @@ typed_skids = pd.merge(left=typed_skids, right=all_skids, on='skid', how='left')
 typed_skids = typed_skids.drop(['paper_name', 'synonyms'], axis=1)
 
 # get FBbt labels from VFB
-FBbt_list = [str(x).replace(':', '_') for x in set(typed_skids['FBbt_id'])]
-query = "MATCH (c:Class) WHERE c.short_form IN %s \
-    RETURN c.short_form AS FBbt_id, c.label AS FBbt_name" % FBbt_list
-
-q = nc.commit_list([query])
-labels = dict_cursor(q)
-labels_df = pd.DataFrame(labels)
-labels_df = labels_df.applymap(lambda x: x.replace('_', ':'))
+FBbt_list = typed_skids['FBbt_id'].drop_duplicates().apply(lambda x: x.replace(':', '_')).to_list()
+query1 = ("MATCH (c:Class) WHERE c.short_form IN %s "
+          "RETURN c.short_form AS FBbt_id, c.label AS FBbt_name" % FBbt_list)
+labels_df = vfb.cypher_query(query1)
+labels_df['FBbt_id'] = labels_df['FBbt_id'].apply(lambda x: x.replace('_', ':'))
 
 # merge FBbt labels into typed skids dataframe on FBbt ID
-typed_skids = pd.merge(left=typed_skids, right=labels_df,
-                       how='left', on='FBbt_id').applymap(str)
+typed_skids = typed_skids.merge(right=labels_df, how='left', on='FBbt_id')
+
 # typed_skids.to_csv("typing.csv", index=None)  # for checking mappings
 
 # drop any annotations that are already in the KB (avoids proliferation of curation files)
 # find all current skids and annotations (copied from comparison.py)
-paper_ids = set(list(typed_skids['paper_id']))
-query = "MATCH (api:API)<-[dsxref:database_cross_reference]-(ds:DataSet)" \
-        "<-[:has_source]-(i:Individual)" \
-        "-[skid:database_cross_reference]->(s:Site) " \
-        "WHERE api.short_form ends with '_catmaid_api' " \
-        "AND s.short_form starts with 'catmaid_' " \
-        "AND dsxref.accession[0] in %s WITH i, skid " \
-        "MATCH (i)-[:INSTANCEOF]-(c:Class) " \
-        "RETURN distinct skid.accession[0] AS `skid`, c.short_form AS `FBbt_id`" \
-        % ('[\'' + '\', \''.join(paper_ids) + '\']')
-
-q = nc.commit_list([query])
-skids_in_paper_vfb = dict_cursor(q)
-vfb_skid_classes_df = pd.DataFrame.from_dict(skids_in_paper_vfb)
-vfb_skid_classes_df = vfb_skid_classes_df.applymap(lambda x: str(x).replace('_', ':'))
+paper_ids = typed_skids.loc[typed_skids['paper_id'].notna(), 'paper_id'].drop_duplicates().to_list()
+query2 = ("MATCH (api:API)<-[dsxref:database_cross_reference]-(ds:DataSet)"
+          "<-[:has_source]-(i:Individual)"
+          "-[skid:database_cross_reference]->(s:Site) "
+          "WHERE api.short_form ends with '_catmaid_api' "
+          "AND s.short_form starts with 'catmaid_' "
+          "AND dsxref.accession[0] in %s WITH i, skid "
+          "MATCH (i)-[:INSTANCEOF]-(c:Class) "
+          "RETURN distinct skid.accession[0] AS `skid`, c.short_form AS `FBbt_id`"
+          % paper_ids)
+vfb_skid_classes_df = vfb.cypher_query(query2)
+vfb_skid_classes_df['FBbt_id'] = vfb_skid_classes_df['FBbt_id'].apply(lambda x: x.replace('_', ':'))
 vfb_skid_classes_df['VFB'] = 'VFB'  # for identifying matches
 
 # merge existing mappings into typed skids df and delete any rows that matched
-all_skid_mappings = pd.merge(
-    left=typed_skids, right=vfb_skid_classes_df, on=['FBbt_id', 'skid'], how='left')
+all_skid_mappings = typed_skids.merge(
+    right=vfb_skid_classes_df, on=['FBbt_id', 'skid'], how='left')
 new_skid_mappings = all_skid_mappings[all_skid_mappings['VFB'].isnull()]  # keep only rows that not in VFB
-new_skid_mappings = new_skid_mappings[new_skid_mappings['paper_id'] != 'nan']  # drop skids not linked to a paper
+new_skid_mappings = new_skid_mappings[new_skid_mappings['paper_id'].notna()]  # drop skids not linked to a paper
 
 # get reference (FBrf or doi) for dataset publications (as dataframe)
 dataset_names = list(set(list(comparison_table['VFB_name'])))  # all ds names
-query2 = ("MATCH (ds:DataSet)-[has_reference]->(p:pub) WHERE ds.short_form IN %s "
+query3 = ("MATCH (ds:DataSet)-[has_reference]->(p:pub) WHERE ds.short_form IN %s "
           "RETURN ds.short_form, p.DOI[0], p.FlyBase[0]"
-          % ('[\'' + '\', \''.join(dataset_names) + '\']'))
-
-q2 = nc.commit_list([query2])
-dataset_refs = dict_cursor(q2)
-dataset_ref_df = pd.DataFrame.from_dict(dataset_refs).set_index('ds.short_form')
+          % dataset_names)
+dataset_ref_df = vfb.cypher_query(query3)
 
 # make a curation record for each dataset
 paper_ids = set(list(new_skid_mappings['paper_id']))
@@ -96,24 +85,15 @@ for i in paper_ids:
         ds = comparison_table['VFB_name'][i]  # dataset name in VFB
     except KeyError:
         continue  # if no row for paper in comparison table
-    if comparison_table['VFB_name'][i] == 'nan':
+    if comparison_table['VFB_name'].isnull()[i]:
         continue  # if no VFB dataset for paper
-    """
-    # no longer adding dataset ref pub for mapping
-    if dataset_ref_df['p.FlyBase'][ds]:
-        ds_ref = 'FlyBase:' + dataset_ref_df['p.FlyBase'][ds]
-    elif dataset_ref_df['p.DOI'][ds]:
-        ds_ref = 'doi:' + dataset_ref_df['p.DOI'][ds]
-    else:
-        ds_ref = ""
-    """
+
     single_ds_data = new_skid_mappings[new_skid_mappings['paper_id'] == i]
 
     # build dataframe of info for curation record
-
     curation_df = pd.DataFrame({'subject_external_db': 'catmaid_fafb',
                                 'subject_external_id': single_ds_data['skid'],
-                                'relation': 'is_a',  # 'pub': ds_ref,
+                                'relation': 'is_a',
                                 'object': single_ds_data['FBbt_name']})
 
     output_filename = './newmeta_%s_%s' % (ds, datestring)
